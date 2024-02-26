d62b253f1a07c5a30b5110e84f314ae1844196ee
CJavatX <cdpm98@hotmail.com>
2024-02-26 11:41:59 -0600

    SECCIÓN 3 - TERMINADA

---
+++ 02-todo/src/app.module.ts
@@ -0,0 +1,24 @@
+import { join } from 'path';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
+import { HelloWorldModule } from './hello-world/hello-world.module';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+// import {  ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      plugins: [
+        ApolloServerPluginLandingPageLocalDefault(), // NUEVA FORMA DE AGREGAR EL APOLLO.
+      ]
+    }),
+    HelloWorldModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
