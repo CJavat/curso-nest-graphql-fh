37cdc62e523187b65dcd20e041458aa0cdfa6cb1
CJavatX <cdpm98@hotmail.com>
2024-03-14 10:30:20 -0600

    SECCIÓN 10 - TERMINADO

--- 03-anylist/src/items/items.service.ts
+++ 03-anylist/src/items/items.service.ts
@@ -1,18 +1,17 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateItemInput, UpdateItemInput } from './dto/inputs';
 import { Item } from './entities/item.entity';
-import { Repository } from 'typeorm';
+import { Like, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
+import { PaginationArgs, SearchArgs } from 'src/common/dto/args';
 
 @Injectable()
 export class ItemsService {
 
   constructor(
-
     @InjectRepository( Item )
     private readonly itemsRepository: Repository<Item>,
-
   ) {}
 
   async create( createItemInput: CreateItemInput, user: User ): Promise<Item> {
@@ -21,15 +20,33 @@ export class ItemsService {
     return await this.itemsRepository.save( newItem );
   }
 
-  findAll( user: User ): Promise<Item[]> {
-    //TODO: filtrar, paginar, por usuario...
-    return this.itemsRepository.find({
-      where: {
-        user: {
-          id: user.id
-        }
-      }
-    });
+  findAll( user: User, paginationArgs: PaginationArgs, searchArgs: SearchArgs ): Promise<Item[]> {
+    const { limit, offset } = paginationArgs;
+    const { search } = searchArgs;
+
+    //? Primera Forma
+    // return this.itemsRepository.find({
+    //   take: limit,
+    //   skip: offset,
+    //   where: {
+    //     user: {
+    //       id: user.id
+    //     },
+    //     name: Like(`%${search}%`),
+    //   }
+    // });
+
+    //* Segunda Forma
+    const queryBuilder = this.itemsRepository.createQueryBuilder()
+      .take( limit )
+      .offset( offset )
+      .where( `"userId" = :userId`, { userId: user.id } );
+
+    if( search ) {
+      queryBuilder.andWhere( `LOWER(name) LIKE :name`, { name: `%${ search.toLowerCase() }%` } );
+    }
+
+    return queryBuilder.getMany();
   }
 
   async findOne( id: string, user: User ): Promise<Item> {
--- 03-anylist/src/users/users.resolver.ts
+++ 03-anylist/src/users/users.resolver.ts
@@ -1,16 +1,15 @@
 import { Resolver, Query, Mutation, Args, Int, ID, ResolveField, Parent } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
-import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { ValidRolesArgs } from './dto/args/roles.arg';
 import { ParseUUIDPipe, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 import { ValidRoles } from 'src/auth/enums/valid-roles.enum';
 import { ItemsService } from 'src/items/items.service';
+import { Item } from 'src/items/entities/item.entity';
+import { PaginationArgs, SearchArgs } from 'src/common/dto/args';
 
 @Resolver(() => User)
 @UseGuards( JwtAuthGuard )
@@ -59,5 +58,15 @@ export class UsersResolver {
     @Parent() user: User
   ): Promise<number> {
     return this.itemsService.itemCountByUser( user );
+  }
+
+  @ResolveField( () => [Item], { name: 'items' } )
+  async getItemsByUser(
+    @CurrentUser([ValidRoles.superuser, ValidRoles.admin ]) adminUser: User,
+    @Parent() user: User,
+    @Args() paginationArgs: PaginationArgs,
+    @Args() searchArgs: SearchArgs,
+  ): Promise<Item[]> {
+    return this.itemsService.findAll( user, paginationArgs, searchArgs );
   }
 }
